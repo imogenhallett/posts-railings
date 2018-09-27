1abec2e930fa931d2a8e4c52c356a9d64e7c9f0b
Imogen <imogenhallett@hotmail.com>
2018-09-27 16:38:22 +0100

    added files for length calculation

---
+++ length.php
@@ -0,0 +1,16 @@
+<?php
+    include_once('functions/length.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Posts and Railings</title>
+</head>
+<body>
+<?php
+    echo 'something goes here';
+?>
+</body>
+</html>
